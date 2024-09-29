94259bdf92597ac19eab330971385af4fb0ebe3b
kwinten <kwintendemaertelaere@gmail.com>
2024-09-29 16:12:48 +0200

    update

--- .config/sway/battery_warning.sh
+++ .config/sway/battery_warning.sh
@@ -7,6 +7,8 @@ if ((battery <= 15 && prev_battery > 15)); then
 	notify-send 'Battery warning!' -u critical "The battery is almost depleted \(${battery}%\)\!"
 elif ((battery <= 30 && prev_battery > 30)); then
 	notify-send 'Battery warning!' "The battery level is low \(${battery}%\)."
+elif ((battery >= 100 && prev_battery < 100)); then
+	notify-send 'Battery' "The battery is completly charged."
 fi
 
 echo "${battery}" > "${prev_battery_file}"
